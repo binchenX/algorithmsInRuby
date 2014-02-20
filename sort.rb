ac9c355cb77fbf1d9051c0de291e2754da613b6d
Pierr Chen <pierr.chen@gmail.com>
2014-02-20 22:11:12 +0800

    impove

--- sort.rb
+++ sort.rb
@@ -1,7 +1,5 @@
 #!/usr/bin/ruby
-#
 def quick_sort a,l,h
-#	puts "qs #{l} #{h}"
 	if ((h-l) > 0)
 		p	=	partition a, l,h
 		quick_sort a, l , p-1
@@ -17,11 +15,9 @@ def swap a , k , i
 end 
 
 def partition a , l , h
-#	puts "part #{l} #{h}"
 	p = h 
 	k = l # the first element >= a[p]
 	for i in l...h
-#		  puts "-> #{a[p]} #{a[i]}"
 			if a[i] < a[p]
 				swap(a , k , i)
 				k = k + 1 
@@ -31,10 +27,26 @@ def partition a , l , h
 	return k
 end
 
-a = [10,4,5,6,7,2,23,45]
-puts a.inspect 
-quick_sort a , 0 , a.size - 1
-puts a.inspect
+def swap a , k , i
+	t = a[k]
+	a[k] = a[i]
+	a[i] = t
+end 
+
+
+
+#tests
+testFail = false
+r = Random.new
+100.times do 
+	#build a array contains 50 element
+	a=[]
+	50.times do a << r.rand(100) end
+	b = a.dup
+	quick_sort a , 0 , a.size - 1
+	if a != b.sort then testFail = true end
+end 
+puts testFail == true ? "fail" : "OK"
 
 
 
