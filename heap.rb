e51b80d0ccee9d295dd9b0ac0d520cb6fb0640bb
Pierr Chen <pierr.chen@gmail.com>
2014-02-23 21:32:55 +0800

    import heap

--- heap.rb
+++ heap.rb
@@ -6,43 +6,50 @@
 #print array in binary tree
 def binary_tree_print a
 	h = (Math.log2 a.size).to_i + 1
-
-	w = 6
-	space = " "
-	seperator = space * w
-	
-#	puts "h is #{h}"
+	base = " " 
 	(1..h).each do |l|
 			t = 2**(l-1)  #number of notes in level l
 			s = if l == 1 then 0 
 					else 
 						(1...l).inject(0) {|sum,i| sum + 2**(i-1)}
 					end
-	
-#			puts "l #{l} -> s #{s} t #{t}"
 			(s...(s+t)).each do |i|	
-				if i < a.size 
-					leading = space*((h - l)*(w/2))
-
-					if s == i then print leading end
-					print a[i] 
-					print seperator
+				if i < a.size
+					if s == i then print base*leading(h,l) end
+					print a[i]  
+					print base*item_space(h,l) 
 				end 
 			end
 			puts "\n"
-			
 	end 
+	puts "-----------------------------"
+end 
+
+
+#the leading of level l of total h
+def leading h, l
+	if h == l then return 0 end
+	leading(h, l + 1) + item_space(h, l + 1) /2  
+end
+
+#the item space in level l
+def item_space h , l
+	base = 4 
+	base * 2** (h - l) 
 end 
 
 #heap 
 def build_max_heap a
-
+	l = a.size
+	(l/2).downto(0).each do |i|
+			max_heapify(a,i)
+	end 
 end 
 
 #assume left(i), right(i) are max-heaps
 def max_heapify a,i 
 
-	s = a.size - 1
+	s = a.size - 1  
 
 	l = left i 
 	r = right i 
@@ -61,6 +68,13 @@ def max_heapify a,i
 
 end
 
+
+def swap a , i ,j 
+	t = a[j]
+	a[j]= a[i]
+	a[i] = t
+	
+end 
 
 #if i started from 1, it will be much simpler
 #left = 2i
@@ -84,19 +98,42 @@ end
 
 #priority queue interface
 # i.e push
-def insert t
+def insert a, k
+
+	a << k
+
 
 
 end 
 
+
+def increaseKey a, i, key
+	return if a[i] >= key
+	a[i] = key
+	puts "increase #{i} to #{key}"
+	#bubble up i until a[i] less then his parent
+	while (i > 0 && a[parent(i)] < a[i]) do
+		swap a, parent(i), i
+		puts "swap #{i} #{parent(i)}"
+		i = parent(i)
+	end
+
+end 
+
+
 # i.e pop
 def extract_max
-
+	max = a[0]
+	#no need to pop actually, we need only record that the heap size decrease on
+	a[0] = a.pop #pop last item
+	heap_size = heap_size - 1
+	max_heapify a, a[0]
+	max
 end 
 
 # i.e peek
 def max a 
-
+	a[0]
 end 
 
 
@@ -109,10 +146,29 @@ def heap_sort a
 end
 
 
-
+=begin
+a = [3,4,6,7,12,5]
+binary_tree_print a 
+max_heapify a , 1 
+puts "after heapify"
+binary_tree_print a 
+=end
 
 a = [3,4,6,7,12,5]
-
 binary_tree_print a 
-puts a.inspect
-#max_heapify a , 1 
+build_max_heap a 
+puts "after build max heap"
+binary_tree_print a 
+puts max a 
+increaseKey a, (a.size-1) ,14
+binary_tree_print a 
+
+=begin
+3.downto(1) do |i|
+puts "space #{item_space 3 ,i} leading #{leading 3,i}"
+end
+
+b=[]
+10.times{|i| b << Random.new.rand(9)}
+binary_tree_print b
+=end
--- sort.rb
+++ sort.rb
@@ -2,11 +2,10 @@
 def quick_sort a,l,h
 	if ((h-l) > 0)
 		p	=	partition a, l,h
-		quick_sort a, l , p-1
-		quick_sort a, p+1,h
+		quick_sort a, l ,   p - 1
+		quick_sort a, p + 1,h
 	end
 end
-
 
 def swap a , k , i
 	t = a[k]
@@ -15,8 +14,8 @@ def swap a , k , i
 end 
 
 def partition a , l , h
-	p = h 
-	k = l # the first element >= a[p]
+	p = h #pick the last as pivot 
+	k = l #the first element >= a[p]
 	for i in l...h
 			if a[i] < a[p]
 				swap(a , k , i)
