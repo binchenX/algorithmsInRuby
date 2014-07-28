dfce61863471cea03c621ed24ea8c78db5cac7cb
Pierr Chen <pierr.chen@gmail.com>
2014-07-28 22:19:58 +0800

    add binary search tree implmentation for symble table

--- testST.rb
+++ testST.rb
@@ -1,34 +1,41 @@
-require_relative 'binarySearchSymbolTable'
+require_relative 'orderedArrayST'
+require_relative 'bst'
 require 'test/unit'
 
-#to use assert
 class TestBinarySearchST <  Test::Unit::TestCase
+	def setup
+		@h = create_st 
+		@refHash = {}
+		k = ['s','e','a','r','c','h','e','x','a','m','p','l','e']
+		v = (0..12).to_a
 
-def test_all
-	h = BinarySearchSymbloTable.new
-	refHash = {}
-	k = ['s','e','a','r','c','h','e','x','a','m','p','l','e']
-	v = (0..12).to_a
-
-	(0..12).each do |i|
-		h.put k[i],v[i]
-		refHash[k[i]] = v[i]
+		(0..12).each do |i|
+			@h.put k[i],v[i]
+			@refHash[k[i]] = v[i]
+		end
 	end
 
-	(0..12).each do |i|
-		key = k[i]
-		puts "#{key} #{refHash[key]} #{h.get(key)}" # then puts "failed!" end
+	def create_st
+		 #OrderedArrayST.new
+			BST.new
 	end
 
-	puts h.keys.sort.inspect
-	assert_equal('a',h.floor('a'))
-	assert_equal('a',h.floor('b'))
-	assert_equal('a',h.ceiling('a'))
-	assert_equal('c',h.ceiling('b'))
+	def test_1
+		@refHash.keys.each do |key|
+			assert_equal(@refHash[key],@h.get(key))
+		end
 
-	assert_equal(true,h.contains?('s'))
-	h.delete('s')
-	assert_equal(false,h.contains?('s'))
+	end
 
-end
+	def test_2
+		assert_equal('a',@h.floor('a'))
+		assert_equal('a',@h.floor('b'))
+		assert_equal('a',@h.ceiling('a'))
+		assert_equal('c',@h.ceiling('b'))
+
+		assert_equal(true,@h.contains?('s'))
+		@h.delete('s')
+		assert_equal(false,@h.contains?('s'))
+
+	end
 end
