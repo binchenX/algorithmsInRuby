87cdb66190f5ef7786c66342f12bc85baf254209
Pierr Chen <pierr.chen@gmail.com>
2014-08-03 17:56:20 +0800

    refactor

--- bst.rb
+++ bst.rb
@@ -1,6 +1,5 @@
 =begin
-Algorithm 4th, Robert Sedgecik
-3.2 Binary Search Tree (BST)
+Binary Search Tree (BST) implementaion in Ruby
 
 Definition: BST is a binary tree where each node has a comparable key 
 and an associated value, with the constraint that key in any node is
@@ -8,6 +7,10 @@ larger  than all the keys of the nodes in that node's left subtree, and
 smaller than all the keys of the nodes in that node's right subtree
 
 BST can be used to implement a sorted Symbol Table(i.e dictionary ) or Set
+
+Reference:
+Algorithm 4th, Robert Sedgecik
+3.2 Binary Search Tree (BST)
 
 copyright pierr.chen AT gmail.com
 =end
@@ -31,7 +34,7 @@ class BST
 	#---------------------------------------
 	#basic  methods: put/get/delete 
 	#helper methods: size/contains/isEmpty
-	#iterators    : keys
+	#iterators     : keys
 	#----------------------------------------
 
 	#insert a new (key,value),
@@ -76,34 +79,29 @@ class BST
 		return nil if x.nil? 
 		#find node to delete
 		r = k <=> x.k
-
-		if (r > 0) then x.right = delete2(x.right, k); updateN x; return x end
-		if (r < 0) then x.left  = delete2(x.left,  k); updateN x; return x end
-
-		#OK. x is node to be deleted
-		#case 1: when the node to be deleted has single child - easy case, exit early
-		if x.left.nil?  then return x.right end
-		if x.right.nil? then return x.left end
-		#case 2:when the node to be deleted has two children
-		#be consistent with the description of delete algorithm in the book
-		t = x
-		#t is the node to be deleted
-		#x is the succssor of t
-		#four steps algorithm of deleting a node with two children
-		x = min2(t.right)
-		x.right = deleteMin2(t.right)
-		x.left = t.left
-		# updae the N 
-		# note that deleteMin2 will update the N of all the nodes in the subtree rooted at [t.right] as necesary
-		# N of the nodes in the subtree rooted at [t.left] has no need to update
+		if (r > 0)    then x.right = delete2(x.right, k)
+		elsif (r < 0) then x.left  = delete2(x.left,  k)
+		else #x is node to be deleted
+			#case 1: when the node to be deleted has single child - easy case, exit early
+			if x.left.nil?  then return x.right end
+			if x.right.nil? then return x.left end
+			#case 2:when the node to be deleted has two children
+			#just to be consistent with the description of delete algorithm in the book
+			t = x
+			#t is the node to be deleted
+			#x is the succssor of t
+			#four steps algorithm of deleting a node with two children
+			x = min2(t.right)
+			x.right = deleteMin2(t.right)
+			x.left = t.left
+		end
 		updateN x
-		return x
+		x
 	end
 
 	def updateN x
 		x.n = size2(x.left) + size2(x.right) + 1
 	end 
-	private :updateN
 
 	def contains? k
 		if get(k).nil? then false else true end
@@ -183,10 +181,12 @@ class BST
 	def ceiling2 n, k
 		return nil if n.nil? 
 	  r = k <=> n.k
-		if (r == 0 ) then return n end
-		if (r > 0  ) then return ceiling2(n.right, k) end
-		t = ceiling2(n.left,k)
-		if not t.nil? then t else n end
+		if    (r == 0 ) then n 
+		elsif (r > 0  ) then ceiling2(n.right, k) 
+		else  
+					t = ceiling2(n.left,k)
+					if not t.nil? then t else n end
+		end
 	end
 
 	#find a key with the given rank 
@@ -200,7 +200,7 @@ class BST
 		r = k <=> t
 		if    (r == 0 )  then x
 		elsif (r < 0  )  then select2(x.left,k) 
-		else                 select2(x.right, k - t - 1)
+		else                  select2(x.right, k - t - 1)
 		end
 	end
 
@@ -234,7 +234,6 @@ class BST
 		n
 	end
 
-
 	# keys in [lo,hi] in sorted order
 	def keysInRange lo, hi
 			keysInRange2 @root, (lo..hi)	
@@ -248,6 +247,5 @@ class BST
 		end
 	end
 	
-	
-	private :size2,:put2,:get2,:floor2,:ceiling2,:delete2,:rank2,:select2
+	private :size2,:put2,:get2,:floor2,:ceiling2,:delete2,:rank2,:select2,:updateN
 end
